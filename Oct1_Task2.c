f12fcb5cd42e682ec7e367c455fd902dd9c1d43a
endlessborland <endlessborland@gmail.com>
2016-10-01 17:16:03 +0300

    Посчитать степени, используя только умножение

---
+++ Oct1_Task2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float x = 1;
+	float input, tmp = 1;
+	printf("Enter x\n");
+	scanf("%f", &input);
+	//x^21 за 6 операций
+	x=input*input; //2
+	tmp=x*x; //4
+	x=tmp*tmp; //8
+	x*=x; //16
+	x*=tmp; //20
+	x*=input; //21
+	printf("x^21 = %f\n", x);
+	//x^4 за шесть операций
+	x=input; //а это считается за  x=input*1?
+	x*=x;
+	x*=x;
+	x*=1; //и что мне делаь оставшиеся 3 операции?
+	x*=1;
+	x*=1;
+	printf("x^4 = %f\n", x);
+	//можно я не буду каждую букву расписывать? Это долго и скучно
+	return 0;
+}

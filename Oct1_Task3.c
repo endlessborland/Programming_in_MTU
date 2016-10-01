bf62582de774c12c3bdf0606023a141244ab244f
endlessborland <endlessborland@gmail.com>
2016-10-01 17:21:58 +0300

    Факториал рекурсией

---
+++ Oct1_Task3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+	if (n == 1)
+		return 1;
+	return n*factorial(n-1);
+}
+
+int main(void)
+{
+	int n;
+	printf("Enter the number\n");
+	scanf("%d", &n);
+	printf("%d! = %d\n", n, factorial(n));
+	return 0;
+}

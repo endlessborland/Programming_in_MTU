38e5a3041153fc591af8964869dd259b3438e5cd
endlessborland <endlessborland@gmail.com>
2016-10-04 02:02:09 +0300

    Testing how Xcode works with Git

---
+++ Test/Test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Test
+//  Github+git+xcode test
+//  Created by Денис Скоробогатов on 04.10.16.
+//  Copyright © 2016 Denis Skorobogatov. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

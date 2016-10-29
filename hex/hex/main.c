25dec71bf1f1ac6360e65dbf0a74402530f0a103
endlessborland <endlessborland@gmail.com>
2016-10-29 13:22:00 +0300

    Some fixes 4 hex

--- hex/hex/main.c
+++ hex/hex/main.c
@@ -26,12 +26,23 @@ int main(int argc, const char * argv[]) {
     int a = 0;
     while ((c = getchar()) != '\n')
     {
-        a = 1;
-        if (checkhex(c) == 0)
+        if (a == 0 && c != '0')
+        {
+            printf("WOW, hex must start with '0x'\n");
+            return 0;
+        }
+        if (a == 1 && c != 'x')
+        {
+            printf("WOW, hex must start with '0x'\n");
+            return 0;
+        }
+        if (checkhex(c) == 0 && a > 1)
         {
             printf("WOW, it's not hex!\n");
             return 0;
         }
+        if (a != 2)
+            a++;
     }
     if (a)
         printf("That looks like hex\n");
